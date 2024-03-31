721ed9fa3fa48d096c3f4e82e02b20e96e089ada
roboter5123 <pascal.roboter@hotmail.com>
2024-03-31 21:37:32 +0200

    Formatting

--- src/main/java/com/frameboter/config/OpenAPIConfig.java
+++ src/main/java/com/frameboter/config/OpenAPIConfig.java
@@ -3,9 +3,8 @@ package com.frameboter.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
